6cba1a8e5d68cc2c2546f1920ae0de4408df28df
subash2devopsintern <subaschy729@gmail.com>
2024-04-26 17:40:35 +0545

    docker permission local sucess modifying in github action

--- scripts/docker-no-sudo.sh
+++ scripts/docker-no-sudo.sh
@@ -14,10 +14,14 @@ if groups $USER | grep &>/dev/null '\bdocker\b'; then
     echo "User is already in the docker group."
 else
     # If not, add the user to the docker group
-    sudo gpasswd -a $USER docker
+    sudo usermod -aG docker $USER
 
-    # Refresh group membership without logging out
-    exec sg docker newgrp
+    # Refresh group membership in the current shell session
+    if [ -z "$GITHUB_ACTIONS" ]; then
+        exec sg docker newgrp
+    else
+        exec newgrp docker
+    fi
 
     echo "User added to the docker group."
 fi
--- scripts/runner-download.sh
+++ scripts/runner-download.sh
@@ -8,17 +8,17 @@ echo -e "Step 1: Folder created.\n----------------------------------------------
 # Download the latest runner package
 echo -e "Step 2: Downloading the latest runner package...\n"
 sudo apt install -y curl
-curl -o actions-runner-linux-x64-2.315.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.315.0/actions-runner-linux-x64-2.315.0.tar.gz
+curl -o actions-runner-linux-x64-2.316.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.316.0/actions-runner-linux-x64-2.316.0.tar.gz
 echo -e "Step 2: Download completed.\n-----------------------------------------------------------------"
 
 # Optional: Validate the hash
 echo -e "Step 3: Validating hash...\n"
-echo  "6362646b67613c6981db76f4d25e68e463a9af2cc8d16e31bfeabe39153606a0  actions-runner-linux-x64-2.315.0.tar.gz" | shasum -a 256 -c
+echo "64a47e18119f0c5d70e21b6050472c2af3f582633c9678d40cb5bcb852bcc18f  actions-runner-linux-x64-2.316.0.tar.gz" | shasum -a 256 -c
 echo -e "Step 3: Hash validated.\n-----------------------------------------------------------------"
 
 # Extract the installer
 echo -e "Step 4: Extracting the installer...\n"
-tar xzf ./actions-runner-linux-x64-2.315.0.tar.gz
+tar xzf ./actions-runner-linux-x64-2.316.0.tar.gz
 echo -e "Step 4: Installer extracted.\n-----------------------------------------------------------------"
 
 echo -e "=================TASK completed Sucessfully======================"
