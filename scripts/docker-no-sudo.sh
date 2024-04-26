f5b3142dd402a724b52f7a214ee3a0a395d79668
subash2devopsintern <subaschy729@gmail.com>
2024-04-26 16:50:23 +0545

    docker run no-sudo : script change

--- scripts/docker-no-sudo.sh
+++ scripts/docker-no-sudo.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+# Check if the docker group exists
+if grep -q '^docker:' /etc/group; then
+    echo "Docker group already exists."
+else
+    # If not, create the docker group
+    sudo groupadd docker
+    echo "Docker group created."
+fi
+
 # Check if the user is already in the docker group
 if groups $USER | grep &>/dev/null '\bdocker\b'; then
     echo "User is already in the docker group."
 else
     # If not, add the user to the docker group
-    sudo groupadd docker
     sudo gpasswd -a $USER docker
     newgrp docker
     echo "User added to the docker group."
